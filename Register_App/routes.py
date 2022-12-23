445cbd2a667c7fff7d4eee159c397f555ad971ca
ericguillaumet <eguillaumet@protonmail.com>
2022-12-23 16:15:31 +0700

    Crear template delete

--- Register_App/routes.py
+++ Register_App/routes.py
@@ -12,6 +12,8 @@ def index():
     #recorrer el objeto csvReader y cargar cada registro a la lista de datos
     for item in csvReader:
         data.append(item)
+    
+    file.close()
 
     return render_template("index.html", pageTitle="Lists", list=data) #Llamamos a la función render template para usar el HTML 
 #El framework solo va a reconocoer el método render_template si index.html se encuentra dentro de la carpeta templates
@@ -60,11 +62,39 @@ def create():
 def edit(id):
     #return render_template("update.html", pageTitle="Edit", typeAction="Modification", typeButton="Edit", dataForm={})
     return f"This is the ID ={id} of the modify register"
-@app.route("/delete/<int:id>")
-def remove(id):
-    return f"This is the ID ={id} of the delete register"
-    #return render_template("delete.html", pageTitle="Delete")
 
+@app.route("/delete/<int:id>", methods=["GET","POST"])
+def remove(id):
+    if request.method == "GET":
+        myfile = open('data/movements.csv', 'r')
+        read = csv.reader(myfile, delimiter=',',quotechar='"')
+        searched_register = []
+        for register in read:
+            if register[0] == str(id):
+                searched_register = register
+        myfile.close()
+        #Si no encuentra registros:
+
+        if len(searched_register) > 0:
+            return render_template("delete.html", pageTitle="Delete", registers = searched_register)
+        else:
+            return redirect("/")
+    else: #aquí sería POST
+        old_file = open('data/movements.csv', 'r')
+        file = open('data/movements.csv', 'w')
+
+        csvReader = csv.reader(old_file, delimiter=',', quotechar='"')
+        csvWriter = csv.writer(file, delimiter=',', quotechar='"')
+
+        for register in csvReader:
+            if register[0] != str(id): #mientras el id sea distinto del proporcionado, para eliminar escribimos encima de "file"
+                csvWriter.writerow(register)
+
+        old_file.close()
+        file.close()
+
+        return redirect("/")
+    
 #Crear una función para validar formulario de registro donde controlemos lo siguiente:
 #1.- que la fecha no sea mayor a la actual.
 #2.- que el concepto no quede vacio.
